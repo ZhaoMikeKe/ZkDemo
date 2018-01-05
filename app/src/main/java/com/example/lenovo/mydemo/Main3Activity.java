fb43f5e222a02bba91be8e59f57501acc2896ed5
zhaoke <514332859@qq.com>
2018-01-05 14:19:28 +0800

    Rotation

--- app/src/main/java/com/example/lenovo/mydemo/Main3Activity.java
+++ app/src/main/java/com/example/lenovo/mydemo/Main3Activity.java
@@ -23,12 +23,21 @@ public class Main3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         ButterKnife.bind(this);
-        Animation an = new RotateAnimation(0.0f, -45.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
-        an.setDuration(1);               // duration in ms
-        an.setRepeatCount(0);                // -1 = infinite repeated
-        an.setRepeatMode(Animation.REVERSE); // reverses each repeat
-        an.setFillAfter(true);               // keep rotation after animation
-        view.setAnimation(an);
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                double a = tv.getHeight();
+                double b = tv.getWidth();
+                double vv = Math.toDegrees(Math.atan2(a, b));
+                Animation an = new RotateAnimation(0.0f, -((float) vv), Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 1f);
+                an.setDuration(1);               // duration in ms
+                an.setRepeatCount(0);                // -1 = infinite repeated
+                an.setRepeatMode(Animation.REVERSE); // reverses each repeat
+                an.setFillAfter(true);               // keep rotation after animation
+                view.setAnimation(an);
+            }
+        });
+
     }
 
     @OnClick({R.id.tv, R.id.view})
