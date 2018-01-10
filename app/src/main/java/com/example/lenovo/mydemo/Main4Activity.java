382641e01f07664336eba14f0d4156f45a555b6c
zhaoke <514332859@qq.com>
2018-01-10 16:51:53 +0800

    全屏

---
+++ app/src/main/java/com/example/lenovo/mydemo/Main4Activity.java
@@ -0,0 +1,33 @@
+package com.example.lenovo.mydemo;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @BindView(R.id.iv)
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        ButterKnife.bind(this);
+        //Imagview全屏化
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        }
+    }
+}
