227dca3f5f733b81632435fbfce770ebe2802d22
zhaoke <514332859@qq.com>
2018-01-23 13:18:28 +0800

    webview

--- webviewtest/src/main/java/com/example/webviewtest/MainActivity.java
+++ webviewtest/src/main/java/com/example/webviewtest/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.webviewtest;
 
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.webkit.WebView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     WebView webview;
@@ -12,7 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webview = (WebView) findViewById(R.id.webview);
-      //Test
-        webview.loadUrl("http://www.baidu.com");
+        //Test
+        //声明WebSettings子类
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setDomStorageEnabled(true); // 开启 DOM storage API 功能
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webview.loadUrl("http://10.3.241.161:8082/ui?systemId=287");
+        //步骤3. 复写shouldOverrideUrlLoading()方法，使得打开网页时不调用系统浏览器， 而是在本WebView中显示
+        webview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+
     }
 }
