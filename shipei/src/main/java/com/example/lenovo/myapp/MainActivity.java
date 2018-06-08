3be5d3427e86127e228d5daa548ea2dcf6b3aad2
zhaoke <514332859@qq.com>
2018-06-08 14:20:08 +0800

    适配刘海屏

--- shipei/src/main/java/com/example/lenovo/myapp/MainActivity.java
+++ shipei/src/main/java/com/example/lenovo/myapp/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.lenovo.myapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DisplayCutout;
+import android.view.View;
+import android.view.WindowManager;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //去掉标题
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        //全屏显示
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        //沉浸式状态栏
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+
+        //下面图1
+        //lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER;
+        //下面图2
+        lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        //下面图3
+        //lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT;
+        getWindow().setAttributes(lp);
+        // getNotchParams();
+        findViewById(R.id.ll_parent).scrollTo(0, -(96));
+    }
+
+    @TargetApi(28)
+    public void getNotchParams() {
+        final View decorView = getWindow().getDecorView();
+
+        decorView.post(new Runnable() {
+            @Override
+            public void run() {
+                DisplayCutout displayCutout = decorView.getRootWindowInsets().getDisplayCutout();
+                Log.e("TAG", "安全区域距离屏幕左边的距离 SafeInsetLeft:" + displayCutout.getSafeInsetLeft());
+                Log.e("TAG", "安全区域距离屏幕右部的距离 SafeInsetRight:" + displayCutout.getSafeInsetRight());
+                Log.e("TAG", "安全区域距离屏幕顶部的距离 SafeInsetTop:" + displayCutout.getSafeInsetTop());
+                Log.e("TAG", "安全区域距离屏幕底部的距离 SafeInsetBottom:" + displayCutout.getSafeInsetBottom());
+
+                List<Rect> rects = displayCutout.getBoundingRects();
+                if (rects == null || rects.size() == 0) {
+                    Log.e("TAG", "不是刘海屏");
+                } else {
+                    Log.e("TAG", "刘海屏数量:" + rects.size());
+                    for (Rect rect : rects) {
+                        Log.e("TAG", "刘海屏区域：" + rect);
+                    }
+                }
+            }
+        });
     }
 }
